d98bd96a664586ec0a8ccad0bc839cab39d7645a
Wellington <juniozguedes@gmail.com>
2022-12-03 15:52:30 -0300

    Finished dependencies change

--- matches_app/admin/wsgi.py
+++ matches_app/admin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tech project.
+WSGI config for matches_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
 
 application = get_wsgi_application()
--- tech/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from blog.models import Team, Stadium, Match
-
-admin.site.register(Team)
-admin.site.register(Stadium)
-admin.site.register(Match)
